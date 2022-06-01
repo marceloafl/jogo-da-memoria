d4af34eda974cfbd1573571da1188de60f870518
Marcelo Lemes <marceloaflemes@gmail.com>
2022-06-01 16:50:40 -0300

    update: versao do jogo com apenas um level

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 const cards = document.querySelectorAll('[data-card]');
+const victoryModal = document.querySelector('[data-modal="victory"]');
+const board = document.querySelector('[data-board]');
+const victoryModalCloseBtn = document.querySelector('[data-victory="close"]');
 let isThereAFlippedCard = false;
 let firstCard;
 let secondCard;
 let lockBoard = false;
+let highlightsCards = 0;
+const cardsInGame = 20;
 
 function flipcard(){
     if(lockBoard) return;
@@ -16,15 +21,25 @@ function flipcard(){
 
     secondCard = this;
     isThereAFlippedCard = false;
+    
     checkMatchCards();
 }
 
 function checkMatchCards(){
     if (firstCard.dataset.card === secondCard.dataset.card ){
+        highlightMatches(firstCard);
+        highlightMatches(secondCard);
         disablecards();
+        checkVictory();
         return;
     }
     unflipCards();
+}
+
+function highlightMatches(card){
+    highlightsCards += 1;
+    card.classList.add('card--highlight')
+    card.dataset.match = 'yes';
 }
 
 function disablecards(){
@@ -49,14 +64,32 @@ function resetBoard(){
     [firstCard, secondCard] = [null, null]
 }
 
+function checkVictory(){
+    console.log(highlightsCards);
+    console.log(cardsInGame);
+    if (highlightsCards === cardsInGame){
+        showVictory();
+    }
+}
+
+function showVictory(){
+    victoryModal.classList.remove('hide');
+    board.classList.add('blur');
+}
+
 (function shuffleCards(){
     cards.forEach(card => {
         let randomPosition = Math.floor(Math.random() * 20)
         card.style.order = randomPosition;
-        console.log(randomPosition)
     })
 })()
 
 cards.forEach(function(card){
     card.addEventListener('click', flipcard);
+})
+
+victoryModalCloseBtn.addEventListener('click', function(){
+    victoryModal.classList.add('hide');
+    board.classList.remove('blur');
+    document.location.reload();
 })
