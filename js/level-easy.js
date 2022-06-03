7fabbe935ff2f5da33edfc9059e5ab1ed7572595
Marcelo Lemes <marceloaflemes@gmail.com>
2022-06-03 10:34:37 -0300

    upload: melhoria de código - criar cartas

--- js/level-easy.js
+++ js/level-easy.js
@@ -5,89 +5,34 @@ export function createLevelEasy(){
     const board = createBoard('easy');
     boardWrapper.appendChild(board)
 
-    const cardHtmlA = createCard('html');
-    const cardHtmlAImg = createFrontImg('html');
-    const cardHtmlAImgBack = createBackImg();
-    cardHtmlA.appendChild(cardHtmlAImg);
-    cardHtmlA.appendChild(cardHtmlAImgBack);
-    board.appendChild(cardHtmlA);
+    createCards('html', board);
+    createCards('html', board);
 
-    const cardHtmlB = createCard('html');
-    const cardHtmlBImg = createFrontImg('html');
-    const cardHtmlBImgBack = createBackImg();
-    cardHtmlB.appendChild(cardHtmlBImg);
-    cardHtmlB.appendChild(cardHtmlBImgBack);
-    board.appendChild(cardHtmlB);
+    createCards('css', board);
+    createCards('css', board);
 
-    const cardCssA = createCard('css');
-    const cardCssAImg = createFrontImg('css');
-    const cardCssAImgBack = createBackImg();
-    cardCssA.appendChild(cardCssAImg);
-    cardCssA.appendChild(cardCssAImgBack);
-    board.appendChild(cardCssA);
+    createCards('javascript', board);
+    createCards('javascript', board);
+    
+    createCards('react', board);
+    createCards('react', board);
+    
+    createCards('github', board);
+    createCards('github', board);
 
-    const cardCssB = createCard('css');
-    const cardCssBImg = createFrontImg('css');
-    const cardCssBImgBack = createBackImg();
-    cardCssB.appendChild(cardCssBImg);
-    cardCssB.appendChild(cardCssBImgBack);
-    board.appendChild(cardCssB);
-
-    const cardJavascriptA = createCard('javascript');
-    const cardJavascriptAImg = createFrontImg('javascript');
-    const cardJavascriptAImgBack = createBackImg();
-    cardJavascriptA.appendChild(cardJavascriptAImg);
-    cardJavascriptA.appendChild(cardJavascriptAImgBack);
-    board.appendChild(cardJavascriptA);
-
-    const cardJavascriptB = createCard('javascript');
-    const cardJavascriptBImg = createFrontImg('javascript');
-    const cardJavascriptBImgBack = createBackImg();
-    cardJavascriptB.appendChild(cardJavascriptBImg);
-    cardJavascriptB.appendChild(cardJavascriptBImgBack);
-    board.appendChild(cardJavascriptB);
-
-    const cardReactA = createCard('react');
-    const cardReactAImg = createFrontImg('react');
-    const cardReactAImgBack = createBackImg();
-    cardReactA.appendChild(cardReactAImg);
-    cardReactA.appendChild(cardReactAImgBack);
-    board.appendChild(cardReactA);
-
-    const cardReactB = createCard('react');
-    const cardReactBImg = createFrontImg('react');
-    const cardReactBImgBack = createBackImg();
-    cardReactB.appendChild(cardReactBImg);
-    cardReactB.appendChild(cardReactBImgBack);
-    board.appendChild(cardReactB);
-
-    const cardGithubA = createCard('github');
-    const cardGithubAImg = createFrontImg('github');
-    const cardGithubAImgBack = createBackImg();
-    cardGithubA.appendChild(cardGithubAImg);
-    cardGithubA.appendChild(cardGithubAImgBack);
-    board.appendChild(cardGithubA);
-
-    const cardGithubB = createCard('github');
-    const cardGithubBImg = createFrontImg('github');
-    const cardGithubBImgBack = createBackImg();
-    cardGithubB.appendChild(cardGithubBImg);
-    cardGithubB.appendChild(cardGithubBImgBack);
-    board.appendChild(cardGithubB);
-
-    const cardGitA = createCard('git');
-    const cardGitAImg = createFrontImg('git');
-    const cardGitAImgBack = createBackImg();
-    cardGitA.appendChild(cardGitAImg);
-    cardGitA.appendChild(cardGitAImgBack);
-    board.appendChild(cardGitA);
-
-    const cardGitB = createCard('git');
-    const cardGitBImg = createFrontImg('git');
-    const cardGitBImgBack = createBackImg();
-    cardGitB.appendChild(cardGitBImg);
-    cardGitB.appendChild(cardGitBImgBack);
-    board.appendChild(cardGitB);
+    createCards('git', board);
+    createCards('git', board);
 
     return board;
+}
+
+function createCards(typeOfCard, board){
+    const card = createCard(typeOfCard);
+    const cardImg = createFrontImg(typeOfCard);
+    const cardImgBack = createBackImg();
+
+    card.appendChild(cardImg);
+    card.appendChild(cardImgBack);
+
+    board.appendChild(card);
 }
--- js/level-hard.js
+++ js/level-hard.js
@@ -5,145 +5,46 @@ export function createLevelHard(){
     const board = createBoard('hard');
     boardWrapper.appendChild(board)
 
-    const cardHtmlA = createCard('html');
-    const cardHtmlAImg = createFrontImg('html');
-    const cardHtmlAImgBack = createBackImg();
-    cardHtmlA.appendChild(cardHtmlAImg);
-    cardHtmlA.appendChild(cardHtmlAImgBack);
-    board.appendChild(cardHtmlA);
+    createCards('html', board);
+    createCards('html', board);
 
-    const cardHtmlB = createCard('html');
-    const cardHtmlBImg = createFrontImg('html');
-    const cardHtmlBImgBack = createBackImg();
-    cardHtmlB.appendChild(cardHtmlBImg);
-    cardHtmlB.appendChild(cardHtmlBImgBack);
-    board.appendChild(cardHtmlB);
+    createCards('css', board);
+    createCards('css', board);
 
-    const cardCssA = createCard('css');
-    const cardCssAImg = createFrontImg('css');
-    const cardCssAImgBack = createBackImg();
-    cardCssA.appendChild(cardCssAImg);
-    cardCssA.appendChild(cardCssAImgBack);
-    board.appendChild(cardCssA);
+    createCards('javascript', board);
+    createCards('javascript', board);
+    
+    createCards('react', board);
+    createCards('react', board);
+    
+    createCards('github', board);
+    createCards('github', board);
 
-    const cardCssB = createCard('css');
-    const cardCssBImg = createFrontImg('css');
-    const cardCssBImgBack = createBackImg();
-    cardCssB.appendChild(cardCssBImg);
-    cardCssB.appendChild(cardCssBImgBack);
-    board.appendChild(cardCssB);
+    createCards('git', board);
+    createCards('git', board);
 
-    const cardJavascriptA = createCard('javascript');
-    const cardJavascriptAImg = createFrontImg('javascript');
-    const cardJavascriptAImgBack = createBackImg();
-    cardJavascriptA.appendChild(cardJavascriptAImg);
-    cardJavascriptA.appendChild(cardJavascriptAImgBack);
-    board.appendChild(cardJavascriptA);
+    createCards('sql', board);
+    createCards('sql', board);
 
-    const cardJavascriptB = createCard('javascript');
-    const cardJavascriptBImg = createFrontImg('javascript');
-    const cardJavascriptBImgBack = createBackImg();
-    cardJavascriptB.appendChild(cardJavascriptBImg);
-    cardJavascriptB.appendChild(cardJavascriptBImgBack);
-    board.appendChild(cardJavascriptB);
+    createCards('java', board);
+    createCards('java', board);
+    
+    createCards('spring', board);
+    createCards('spring', board);
 
-    const cardReactA = createCard('react');
-    const cardReactAImg = createFrontImg('react');
-    const cardReactAImgBack = createBackImg();
-    cardReactA.appendChild(cardReactAImg);
-    cardReactA.appendChild(cardReactAImgBack);
-    board.appendChild(cardReactA);
-
-    const cardReactB = createCard('react');
-    const cardReactBImg = createFrontImg('react');
-    const cardReactBImgBack = createBackImg();
-    cardReactB.appendChild(cardReactBImg);
-    cardReactB.appendChild(cardReactBImgBack);
-    board.appendChild(cardReactB);
-
-    const cardGithubA = createCard('github');
-    const cardGithubAImg = createFrontImg('github');
-    const cardGithubAImgBack = createBackImg();
-    cardGithubA.appendChild(cardGithubAImg);
-    cardGithubA.appendChild(cardGithubAImgBack);
-    board.appendChild(cardGithubA);
-
-    const cardGithubB = createCard('github');
-    const cardGithubBImg = createFrontImg('github');
-    const cardGithubBImgBack = createBackImg();
-    cardGithubB.appendChild(cardGithubBImg);
-    cardGithubB.appendChild(cardGithubBImgBack);
-    board.appendChild(cardGithubB);
-
-    const cardGitA = createCard('git');
-    const cardGitAImg = createFrontImg('git');
-    const cardGitAImgBack = createBackImg();
-    cardGitA.appendChild(cardGitAImg);
-    cardGitA.appendChild(cardGitAImgBack);
-    board.appendChild(cardGitA);
-
-    const cardGitB = createCard('git');
-    const cardGitBImg = createFrontImg('git');
-    const cardGitBImgBack = createBackImg();
-    cardGitB.appendChild(cardGitBImg);
-    cardGitB.appendChild(cardGitBImgBack);
-    board.appendChild(cardGitB);
-
-    const cardSqlA = createCard('sql');
-    const cardSqlAImg = createFrontImg('sql');
-    const cardSqlAImgBack = createBackImg();
-    cardSqlA.appendChild(cardSqlAImg);
-    cardSqlA.appendChild(cardSqlAImgBack);
-    board.appendChild(cardSqlA);
-
-    const cardSqlB = createCard('sql');
-    const cardSqlBImg = createFrontImg('sql');
-    const cardSqlBImgBack = createBackImg();
-    cardSqlB.appendChild(cardSqlBImg);
-    cardSqlB.appendChild(cardSqlBImgBack);
-    board.appendChild(cardSqlB);
-
-    const cardJavaA = createCard('java');
-    const cardJavaAImg = createFrontImg('java');
-    const cardJavaAImgBack = createBackImg();
-    cardJavaA.appendChild(cardJavaAImg);
-    cardJavaA.appendChild(cardJavaAImgBack);
-    board.appendChild(cardJavaA);
-
-    const cardJavaB = createCard('java');
-    const cardJavaBImg = createFrontImg('java');
-    const cardJavaBImgBack = createBackImg();
-    cardJavaB.appendChild(cardJavaBImg);
-    cardJavaB.appendChild(cardJavaBImgBack);
-    board.appendChild(cardJavaB);
-
-    const cardSpringA = createCard('spring');
-    const cardSpringAImg = createFrontImg('spring');
-    const cardSpringAImgBack = createBackImg();
-    cardSpringA.appendChild(cardSpringAImg);
-    cardSpringA.appendChild(cardSpringAImgBack);
-    board.appendChild(cardSpringA);
-
-    const cardSpringB = createCard('spring');
-    const cardSpringBImg = createFrontImg('spring');
-    const cardSpringBImgBack = createBackImg();
-    cardSpringB.appendChild(cardSpringBImg);
-    cardSpringB.appendChild(cardSpringBImgBack);
-    board.appendChild(cardSpringB);
-
-    const cardQaA = createCard('qa');
-    const cardQaAImg = createFrontImg('qa');
-    const cardQaAImgBack = createBackImg();
-    cardQaA.appendChild(cardQaAImg);
-    cardQaA.appendChild(cardQaAImgBack);
-    board.appendChild(cardQaA);
-
-    const cardQaB = createCard('qa');
-    const cardQaBImg = createFrontImg('qa');
-    const cardQaBImgBack = createBackImg();
-    cardQaB.appendChild(cardQaBImg);
-    cardQaB.appendChild(cardQaBImgBack);
-    board.appendChild(cardQaB);
+    createCards('qa', board);
+    createCards('qa', board);
 
     return board;
+}
+
+function createCards(typeOfCard, board){
+    const card = createCard(typeOfCard);
+    const cardImg = createFrontImg(typeOfCard);
+    const cardImgBack = createBackImg();
+
+    card.appendChild(cardImg);
+    card.appendChild(cardImgBack);
+
+    board.appendChild(card);
 }
